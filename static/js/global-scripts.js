4f8d607159c2b0eecbf2000953e60b6173b8f7cc
sebam2k4 <sebam2k4@gmail.com>
2018-03-20 12:28:52 +0000

    remove console logs & duplicate code

--- static/js/global-scripts.js
+++ static/js/global-scripts.js
@@ -1,16 +1,17 @@
+
+
+///////////////////////////////////////
+// Display server messages in front-end
+
 if (document.querySelector('.auth-messages')) {
-  // Clear Error Message after delay
-  console.log("messages div found")
-  // 3rd parameter of the setTimeout function is
-  // sent as parameter to the internal function at
-  // end of the timer
+  /* Clear Error Message after delay
+    note: 3rd parameter of the setTimeout function is
+    sent as parameter to the internal function at
+    end of the timer */
   setTimeout(clearMessage, 3000, '.auth-messages')
   }
   
-// Clear Messages
-function clearMessage(message){
-  document.querySelector(message).remove();
-}// Clear Messages
+// Clear Messages reusable method
 function clearMessage(message){
   document.querySelector(message).remove();
 }
