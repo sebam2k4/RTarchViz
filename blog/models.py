de70543cd0aca2a3f80f4880cee850549c300b8c
sebam2k4 <sebam2k4@gmail.com>
2018-03-20 23:40:32 +0000

    import timezone

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.utils import timezone
 
 
 
