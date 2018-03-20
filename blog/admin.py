0190248d6690aedd361fa5a61370f6dc949e4888
sebam2k4 <sebam2k4@gmail.com>
2018-03-20 23:41:26 +0000

    create custom Admin for Blog Posts

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Post
 
+class PostAdmin(admin.ModelAdmin):
+  list_display = ("title", "author", "published_date", "status", "category")
+  list_filter = ("status", "created_date", "published_date", "updated_date")
+  search_fields = ("title", "author")
+  date_hierarchy = "published_date"
+  ordering = ["status", "published_date"]
+  readonly_fields=("created_date", "published_date", "updated_date", "views_count", "slug",)
 # Register your models here.
+admin.site.register(Post, PostAdmin)
