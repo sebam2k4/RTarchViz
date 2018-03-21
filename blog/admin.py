ebb281233af76804f2abf2a53ccb5dc4a1057b66
sebam2k4 <sebam2k4@gmail.com>
2018-03-21 00:19:22 +0000

    Adjust ordering & filtering in for Admin

--- blog/admin.py
+++ blog/admin.py
@@ -5,11 +5,11 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-  list_display = ("title", "author", "published_date", "status", "category")
-  list_filter = ("status", "created_date", "published_date", "updated_date")
+  list_display = ("title", "author", "published_date", "updated_date", "status", "category")
+  list_filter = ("status", "category", "created_date", "published_date", "updated_date")
   search_fields = ("title", "author")
   date_hierarchy = "published_date"
-  ordering = ["status", "published_date"]
+  ordering = ["status", "-published_date"]
   readonly_fields=("created_date", "published_date", "updated_date", "views_count", "slug",)
 # Register your models here.
 admin.site.register(Post, PostAdmin)
