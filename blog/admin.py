6e427a75fb507fb6317065592c4065141682a824
sebam2k4 <sebam2k4@gmail.com>
2018-03-21 15:22:11 +0000

    improve admin layout

--- blog/admin.py
+++ blog/admin.py
@@ -3,13 +3,22 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from .models import Post
+from django.utils.translation import gettext, gettext_lazy as _
 
 class PostAdmin(admin.ModelAdmin):
-  list_display = ("title", "author", "published_date", "updated_date", "status", "category")
+  list_display = ("title", "author", "published_date", "updated_date", "status", "category", "views_count")
   list_filter = ("status", "category", "created_date", "published_date", "updated_date")
   search_fields = ("title", "author")
   date_hierarchy = "published_date"
   ordering = ["status", "-published_date"]
+
   readonly_fields=("created_date", "published_date", "updated_date", "views_count", "slug",)
+  fieldsets = (
+                (None,          {'fields': ('title', 'slug', 'content', 'category')}),
+                (_('Status'),   {'fields': ('status',)}),
+                (_('History'),  {'fields': ('created_date', 'published_date', 'updated_date')}),
+                (_('Views'),  {'fields': ('views_count',)}),
+              )
+    
 # Register your models here.
 admin.site.register(Post, PostAdmin)
