2f9a81a156f4c523412747da5404d1ae18d5b5f8
sebam2k4 <sebam2k4@gmail.com>
2018-03-23 13:15:32 +0000

    limit post  author choices to staff only

--- blog/admin.py
+++ blog/admin.py
@@ -4,9 +4,20 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from django.db import models
 from .models import Post
+from django.contrib.auth import get_user_model
 from django.utils.translation import gettext, gettext_lazy as _
 
 class PostAdmin(admin.ModelAdmin):
+  def formfield_for_foreignkey(self, db_field, request, **kwargs):
+    """
+    override default formfield for a foreign key field (author) to
+    return only users who are staff
+    """
+    if db_field.name == 'author':
+      # use get_user_model to return current active user model instead of 'import accounts.User'
+      kwargs['queryset'] = get_user_model().objects.filter(is_staff=request.user.is_staff)
+    return super(PostAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
   list_display = ("title", "author", "published_date", "updated_date", "status", "category", "view_count")
   list_filter = ("status", "category", "created_date", "published_date", "updated_date")
   search_fields = ("title", "author")
