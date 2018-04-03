7a98133d4c5a5a52f996d8e1eab2fa853d3466b2
sebam2k4 <sebam2k4@gmail.com>
2018-04-03 23:27:13 +0100

    modify custom recent_posts tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -5,10 +5,11 @@ register = template.Library()
 
 
 @register.inclusion_tag('_homepage_recent_posts.html')
-def home_recent_posts():
+def home_recent_posts(num):
   """
-  Return the last 5 blog posts in a partial template specifically for
-  inclusion into the Homepage App index.html template or a landing page.
+  Return the specified number of recent blog posts in a partial template
+  specifically for inclusion into the Homepage App index.html template
+  or a landing page.
   """
-  recent_posts = Post.objects.published()[:5]
+  recent_posts = Post.objects.published()[:num]
   return {'recent_posts': recent_posts}
