3003d06bd84f23a3569bd601dc917e577c5696e7
sebam2k4 <sebam2k4@gmail.com>
2018-04-08 11:32:51 +0100

    Register Checkout models in admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,5 +2,19 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Order, OrderProduct
+from django.utils.translation import gettext, gettext_lazy as _
+  
+class OrderProductAdminInline(admin.TabularInline):
+  fields = ('product',)
+  model = OrderProduct
+  extra = 0
+  
 
-# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+  inlines = (OrderProductAdminInline, )
+  exclude = ('products',)
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderProduct)
