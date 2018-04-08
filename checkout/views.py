8050e149044d9fcd1601bfbc6df8e539eec95c8f
sebam2k4 <sebam2k4@gmail.com>
2018-04-08 11:31:58 +0100

    Create 'Payment Form' & 'Order Form'

--- checkout/views.py
+++ checkout/views.py
@@ -3,11 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import MakePaymentForm, OrderForm
 
 # Create your views here.
 
 @login_required
 def checkout(request):
-  context = {}
+  payment_form = MakePaymentForm()
+  order_form = OrderForm()
+  context = {'payment_form': payment_form, 'order_form': order_form}
 
   return render(request, "checkout.html", context)
