2392cad9144dbe503a074a2bbb766297e782e665
sebam2k4 <sebam2k4@gmail.com>
2018-04-08 13:05:48 +0100

    Add order_total method and code comments

--- checkout/models.py
+++ checkout/models.py
@@ -8,22 +8,32 @@ from products.models import Product
 # Create your models here.
 
 class Order(models.Model):
+  """ Defining Order models """
+
+  # DATABASE FIELDS
   buyer = models.ForeignKey(settings.AUTH_USER_MODEL)
-  ordered_date = models.DateTimeField()
-  product_count = models.IntegerField(blank=False, null=False)
-  total_amount = models.DecimalField(max_digits=8, decimal_places=2)
+  ordered_date = models.DateTimeField(editable=False)
+  product_count = models.IntegerField(editable=False, blank=False, null=False)
+  total_amount = models.DecimalField(editable=False, max_digits=8, decimal_places=2)
   products = models.ManyToManyField(Product, through='OrderProduct', related_name='ordered_products')
 
-  def __str__(self):
+  # TO STRING METHOD
+  def __unicode__(self):
     return "Order #{0}, {1} ----- Order made by: {2}".format(self.id, self.ordered_date, self.buyer.username)
+
+  # MODEL METHODS
+  def order_total(self):
+    return '€{0}'.format(self.total_amount)
+
 
 # Intermediate model for m-m relationship between Order and Product
 class OrderProduct(models.Model):
-  """
-  Define Product line 
-  """
+  """ Defining Order Product models """
+
+  # DATABASE FIELDS
   order = models.ForeignKey(Order, null=False)
   product = models.ForeignKey(Product, null=False)
 
-  def __str__(self):
+  # TO STRING METHOD
+  def __unicode__(self):
     return "{0} by {1} {2} ---- purchased by {3}".format(self.product.name, self.product.seller.username, self.product.price, self.order.buyer.username)
