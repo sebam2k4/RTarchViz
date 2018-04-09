0a75dc828c21ffe1ad05a94b2e492da89c595c81
sebam2k4 <sebam2k4@gmail.com>
2018-04-09 23:16:26 +0100

    Create Homepage unit tests

--- homepage/tests.py
+++ homepage/tests.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
-
+from django.contrib.auth import get_user_model
+from blog.models import Post
 # Create your tests here.
+
+class HomePageTests(TestCase):
+
+  def test_homepage(self):
+    response = self.client.get('/')
+    self.assertEqual(response.status_code, 200)
