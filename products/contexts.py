6c387d0b5d9d27210afde05e1804e4cccc2a22b4
sebam2k4 <sebam2k4@gmail.com>
2018-04-09 01:35:03 +0100

    update context processors (cart & owned products)

--- products/contexts.py
+++ products/contexts.py
@@ -4,14 +4,12 @@ from checkout.models import Order
 
 def owned_assets(request):
   """
-  Make cart contents available to all apps and derive some useful data
-  to use in cart and other templates
+  Make user owned(purchased) products available to all apps
   """
-  # retrieve session key for cart and its contents in a dictionary
   orders = Order.objects.filter(buyer_id=request.user.id)
   owned_assets = []
   for order in orders:
     for item in order.products.all():
       owned_assets.append(item)
-  print owned_assets
+  print "owned Assets: {0}".format(owned_assets)
   return {'owned_assets': owned_assets}
