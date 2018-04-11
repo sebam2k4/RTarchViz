5bfb36d8013db6524ae482fcb36f0e01572889c8
sebam2k4 <sebam2k4@gmail.com>
2018-04-12 00:04:48 +0100

    Add more blog integration and unit tests

--- blog/tests.py
+++ blog/tests.py
@@ -6,6 +6,7 @@ from .models import Post
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.utils.text import slugify
+from django.urls import reverse
 
 
 class PostModelTest(TestCase):
@@ -22,9 +23,20 @@ class PostModelTest(TestCase):
 
 class BlogPostListViewTest(TestCase):
   """ Tests for Blog Post List view """
-  def test_blog_list_page(self):
+  def test_view_url_exists_at_desired_location(self):
     response = self.client.get('/blog/')
     self.assertEqual(response.status_code, 200)
+
+  def test_view_url_accessible_by_name(self):
+    response = self.client.get(reverse('posts_list'))
+    self.assertEqual(response.status_code, 200)
+  
+  def test_view_uses_correct_template(self):
+      response = self.client.get(reverse('posts_list'))
+      self.assertEqual(response.status_code, 200)
+      self.assertTemplateUsed(response, 'base.html')
+      self.assertTemplateUsed(response, 'posts_list.html')
+      
 
   """Test whether blog posts show up on the Blog list view"""
 
@@ -61,16 +73,44 @@ class BlogPostListViewTest(TestCase):
     self.assertNotContains(response, '6-title')
     self.assertNotContains(response, '6-content')
 
+  """ test pagination for Blog list view """
+
+  @classmethod
+  def setUpTestData(cls):
+    #Create 11 posts for pagination tests
+    author = get_user_model().objects.create(username='author1', email='author1@gmail.com')
+    number_of_posts = 11
+    for post_num in range(number_of_posts):
+      Post.objects.create(author=author, title='title {0}'.format(post_num), content='content {0}'.format(post_num), status='published')
+
+  def test_pagination_is_4(self):
+    response = self.client.get(reverse('posts_list'))
+    self.assertEqual(response.status_code, 200)
+    # total items/page
+    self.assertTrue( len(response.context['posts']) == 4)
+
+  def test_lists_all_posts(self):
+    #Get third page and confirm it has (exactly) remaining 3 items
+    response = self.client.get(reverse('posts_list')+'?page=4')
+    self.assertEqual(response.status_code, 200)
+    self.assertTrue( len(response.context['posts']) == 3)
+
 
 class BlogPostDetailViewTest(TestCase):
   """ Tests for Blog Post Detail view """
   def setUp(self):
     self.user = get_user_model().objects.create(username='test_user', email='test_user@gmail.com')
-    self.post = Post.objects.create(title='1-title', content='1-content', published_date=timezone.now(), slug='1-title', author=self.user)
+    self.post = Post.objects.create(title='1-title', content='1-content', status='published', slug='1-title', author=self.user)
 
-  def test_detail_view(self):
+  def test_view_url_exists_at_desired_location(self):
     response = self.client.get(self.post.get_absolute_url())
     self.assertEqual(response.status_code, 200)
+
+  def test_view_uses_correct_template(self):
+    response = self.client.get(self.post.get_absolute_url())
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'base.html')
+    self.assertTemplateUsed(response, 'post_detail.html')
 
   def test_title_in_post(self):
     response = self.client.get(self.post.get_absolute_url())
@@ -79,3 +119,4 @@ class BlogPostDetailViewTest(TestCase):
   def test_content_in_post(self):
     response = self.client.get(self.post.get_absolute_url())
     self.assertContains(response, self.post.content)
+
